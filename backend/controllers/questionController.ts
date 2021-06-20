8fb4f170c84a8d5a1244a2116d9db5c7408856b6
Dhruvang1996 <dhruvang.merchant@ucalgary.ca>
2021-06-20 00:02:57 -0600

    Feat: Implement Bulk questions from the id

--- backend/controllers/questionController.ts
+++ backend/controllers/questionController.ts
@@ -6,7 +6,7 @@ export const getAllQuestions = async (req: Request, res: Response): Promise<void
     const allQuestions = await Questions.find();
     res.status(200).send(allQuestions);
   } catch (error) {
-    res.status(404).send({ error });
+    res.status(500).send({ error });
   }
 };
 
@@ -15,7 +15,7 @@ export const getQuestionById = async (req: Request, res: Response): Promise<void
     const questionById = await Questions.findById(req.params['id']);
     res.status(200).send(questionById);      
   } catch (error) {
-    res.status(404).send({ error });
+    res.status(500).send({ error });
   }
 };
 
@@ -24,6 +24,15 @@ export const addQuestion = async (req: Request, res: Response): Promise<void> =>
     const newQuestion = await Questions.create(req.body); 
     res.status(201).send(newQuestion._id);
   } catch (error) {
-    res.status(404).send({ error });
+    res.status(500).send({ error });
+  }
+};
+
+export const getBulkQuestions = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const bulkQuestions = await Questions.find({ _id: { $in: req.body.questions }});
+    res.status(200).send(bulkQuestions);
+  } catch (error) {
+    res.status(500).send({ error });
   }
 };
